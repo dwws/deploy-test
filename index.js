505d763e530a28a0e3596ab862d0facf397fc737
dfgfdg aewqf <albert.rahmatullin@distate.dsnet>
2023-03-03 22:13:29 +0400

    git ignore

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.status(200).send('Ok'));
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
